36514e8f749515336f544a5cc5ffcb65e65f8e19
muhamadayeshaaulia <shaxyyy.03@gmail.com>
2025-05-05 17:27:11 +0700

    update User

--- app/Models/User.php
+++ app/Models/User.php
@@ -54,7 +54,7 @@ class User extends Authenticatable
  */
     public function getPermissionArray()
     {
-        return $this->getAllPermissions()->maoWithKeys(function ($pr){
+        return $this->getAllPermissions()->mapWithKeys(function ($pr){
             return [$pr['name']=>true];
         });
     }
